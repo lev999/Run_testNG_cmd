60f5989cd7931d7e8e45a07e5777b4ed61e70b36
Lev999 <lev.magazinnik@gmail.com>
2015-06-23 11:20:17 -0300

    Initial commit

---
+++ RUN_testNg_cmd/src/Main.java
@@ -0,0 +1,34 @@
+
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+// to standart testng-6.8.21.jar need to add jcommander-1.7.jar!
+
+
+// to run in cmd: java -cp run.Main
+// java - cp "./path fo .class;./path to lib" run.Main
+
+
+//-------------to run TEST ----------
+//java org.testng.TestNG %ProjectPath%\testng.xml
+//
+//        set projectPath=F:\Lev_PRIVATE_data\JAVA_projects\REAL_PROJECT\QA\Genefo\target\MyTest
+//        set classpath=%Projectpath%\bin\*;%ProjectPath%\lib\*
+//
+//        set classpath=F:\Lev_PRIVATE_data\JAVA_projects\REAL_PROJECT\QA\kill\untitled
+//        java -cp ".;F:\Lev_PRIVATE_data\JAVA_projects\REAL_PROJECT\QA\kill\untitled\testng-6.8.21.jar" org.testng.TestNG testng.xml
+//-------------------------------------------------
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world! 22");
+//        org.testng.TestNG.main(new String[]{"testng.xml"});
+    }
+    @Test
+    public void myTest(){
+        Assert.assertTrue(true);
+    }
+
+
+}
